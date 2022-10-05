6a7966862413048d3307ccc64757d39bd4f874e0
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-10-05 17:29:33 -0400

    Add type enum

---
+++ src/A0009-type-enum.ts
@@ -0,0 +1,21 @@
+// Type enum
+// Estrutura de dados não ordenada
+
+enum Cores { //Sempre letra maiúsculas
+  VERMELHO, //0
+  AZUL, //1
+  AMARELO = 200, //200 Setando manualmente
+  ROXO = 'ROXO',
+}
+
+console.log(Cores);
+console.log(Cores.VERMELHO);
+console.log(Cores[0]);
+
+enum Cores { //Merge de ENUMS, vai se misturar com o de cima
+  ROSA = 'ROSA',
+}
+// Ex:
+function chooseColor(color: Cores): void {
+  console.log(Cores[color]);
+}
