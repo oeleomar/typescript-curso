a03e7f49c720a872ac1c6b42a88a806c32f22f1e
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-10-25 17:29:22 -0400

    ok

---
+++ A002-module-POO/src/A0014-interfaces/A0014-interfaces.ts
@@ -0,0 +1,26 @@
+//Interface
+
+type TipoPessoa = {
+  nome: string;
+  sobrenome: string;
+  nomeCompleto(): string;
+};
+
+//Posso implementar todos os tipos
+type CPF = {
+  cpf: string;
+};
+
+export class Pessoa implements TipoPessoa, CPF {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    public cpf: string
+  ) {}
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa1 = new Pessoa('Eleomar', 'Dorneles', '000.000.000-00');
+console.log(pessoa1.nomeCompleto);
