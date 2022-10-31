593a48f60300b273ed43cd8f36a3cd1d4731dde1
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-10-31 14:35:35 -0400

    Add decorators 5

---
+++ A002-module-POO/src/A0033-decorators-method/A0033-decorators-method.ts
@@ -0,0 +1,47 @@
+//Decorators de method
+//Posso decorar properties, methods
+
+function decorador(
+  classPrototype: any,
+  nomeMethod: string,
+  descriptor: PropertyDescriptor
+): any {
+  console.log(classPrototype);
+  console.log(nomeMethod);
+  console.log(descriptor);
+
+  return {
+    value: function (...args: string[]) {
+      return args[0].toUpperCase();
+    },
+  };
+}
+
+export class UmaPesso {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  @decorador
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
